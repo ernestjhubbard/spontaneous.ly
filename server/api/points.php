048f0ba85f91efbd19db6b3b63dd3826958c5c28
ernest hubbard <ernhub89@gmail.com>
2019-11-27 21:18:33 -0800

    transactions successful

--- server/api/points.php
+++ server/api/points.php
@@ -1,14 +1,40 @@
 <?php
 $link = get_db_link();
 
-if ($request['method'] === 'GET') {
-    $query =   "SELECT points FROM activities";
-    $result = mysqli_query($link, $query);
-    $output = [];
-    $count = 0;
-    while($row = mysqli_fetch_assoc($result)){
-      $count += $row['points'];
+if ($request['method'] === 'POST') {
+  $user_id = 1;
+  $transaction_type = $request['body']['transactionType'];
+  if($transaction_type ==="reservation" && isset($request['body']['activityId'])){
+    print('what the fuck');
+      $activity_id = $request['body']['activityId'];
+      $transaction = "reservation";
+      $sql_points = "SELECT points 
+                       FROM activities
+                      WHERE activityId = $activity_id";
+      $points_query = mysqli_query($link, $sql_points);
+      $points = mysqli_fetch_assoc($points_query);
+      $point_value = $points['points'];
+      $sql_transaction = "INSERT INTO points (`userId`, `value`, `transactionType`) 
+                               VALUES ($user_id, $point_value, '$transaction_type')";
+      mysqli_query($link, $sql_transaction);
+      $response['body'] = $points;
+      send($response);
     }
-    $response['body'] = $count;
+  else{
+    if($transaction_type === "re-roll"){
+      $sql_reroll = "INSERT INTO points (`userId`, `value`, `transactionType`) 
+                          VALUES ($user_id, -25, '$transaction_type')";
+      mysqli_query($link, $sql_reroll);
+    }
+    else if($transaction_type === "cancellation"){
+      $sql_cancel = "INSERT INTO points (`userId`, `value`, `transactionType`) 
+                          VALUES ($user_id, -50, '$transaction_type')";
+      mysqli_query($link, $sql_cancel);
+    }
+    else{
+      throw new ApiError("Invalid Transaction Type", 400);
+    }
+    $response['body'] = "Post Successful";
     send($response);
+  }
 }
