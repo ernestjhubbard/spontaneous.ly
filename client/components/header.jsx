3090c4a2a82775b76e57f71ae920943aff13f643
Vicky Huynhcao <vicky89@gmail.com>
2019-11-28 01:24:52 +0000

    Removed flex on CSS and added d-flex in header.jsx

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import MenuNav from './menu-nav.jsx';
 
 function Header(props) {
   return (
-    <div className="header-bar position-static">
+    <div className="header-bar d-flex position-static">
       <img className="logo-alone mt-3 ml-4"
         src="assets/images/logo-alone.png"
         onClick={() => props.setView('home')}/>
