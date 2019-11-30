c71d4689f4e606ba5ed8f0a551bc43234e86fda4
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-11-30 01:38:39 +0000

    fixing the carousel after merge conflict

--- client/components/activity-list.jsx
+++ client/components/activity-list.jsx
@@ -5,32 +5,10 @@ class ActivityList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activities: []
     };
-    this.getAllActivities = this.getAllActivities.bind(this);
-  }
-
-  componentDidMount() {
-    this.getAllActivities();
-  }
-
-  getAllActivities() {
-    fetch('api/all-activities')
-      .then(response => response.json())
-      .then(activityList => {
-        const listedActivities = this.state.activities.concat(activityList);
-        this.setState({ activities: listedActivities });
-      })
-      .catch(error => console.error('Fetch error: ', error));
-
   }
 
   render() {
-    var activityCard = this.state.activities.map(activity => {
-      return (
-        <Carousel key={activity.activityId} id={activity.activityId} image={activity.image} name={activity.activity} />
-      );
-    });
     return (
       <div className="">
         <div className="top-banner d-flex">
@@ -40,7 +18,7 @@ class ActivityList extends React.Component {
           </div>
         </div>
         <div className="container-fluid my-5">
-          {activityCard}
+          <Carousel />
         </div>
       </div>
     );
