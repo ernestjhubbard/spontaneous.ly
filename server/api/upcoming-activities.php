60442e116cf2f707981d94eeb246ed418c6d8680
juliehchung <juliehchung96@gmail.com>
2019-12-02 18:59:14 +0000

    Completed back end feature where user can get upcoming activities they are registered to attend, in order

--- server/api/upcoming-activities.php
+++ server/api/upcoming-activities.php
@@ -4,14 +4,19 @@ $link = get_db_link();
 
 if ($request['method'] === 'GET') {
   $user_id = 1;
-  $sql_reserved_activities = "SELECT reservations.activityId, activities.activity, activities.date, activities.time, activities.points, activities.image
+  $sql_reserved_activities = "SELECT reservations.activityId, activities.activity, activities.dateTime, activities.points, activities.image
                                 FROM reservations
                                 JOIN activities
                                   ON activities.activityId = reservations.activityId
                                WHERE userId = $user_id
-                                 AND isCancelled = 0";
+                                 AND activities.dateTime > NOW()
+                                 AND isCancelled = 0
+                            ORDER BY activities.dateTime ASC";
   $reserved_activities_query = mysqli_query($link, $sql_reserved_activities);
   $upcoming_activities = mysqli_fetch_all($reserved_activities_query, MYSQLI_ASSOC);
+
+
+
   $response['body'] = $upcoming_activities;
   send($response);
 }
