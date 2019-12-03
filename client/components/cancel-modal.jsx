1f8688efb74f9e247c94ff2f33bc6b8719f8d4db
Vicky Huynhcao <vicky89@gmail.com>
2019-12-03 19:01:24 +0000

    Styling finished. Going to work on posting the cancel request

--- client/components/cancel-modal.jsx
+++ client/components/cancel-modal.jsx
@@ -6,13 +6,13 @@ class CancelModal extends React.Component {
     return (
       <div className="confirm-cancel-modal">
         <div className="inner-modal text-center">
-          <h1>Are you sure you want to cancel your reservation?</h1>
+          <h1 className="mt-2">Are you sure you want to cancel your reservation?</h1>
           <h5>
             Cancelling costs you <span className="font-color">50</span>{' '}
             Spontaneity Points
           </h5>
           <button className="cancel-confirm-button">Confirm</button>
-          <button className="cancel-back-button" onClick={this.props.closeModal}>Back</button>
+          <button className="cancel-back-button ml-2" onClick={this.props.closeModal}>Back</button>
         </div>
       </div>
     );
