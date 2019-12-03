f55d665b90eacc2441ee9dc36f4abf3ecbc95d03
juliehchung <juliehchung96@gmail.com>
2019-12-03 02:44:21 +0000

    Added functionality to display date and time in user-friendly format

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -13,7 +13,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 'friendPage',
+      view: 'profilePage',
       messages: [],
       user: {
         firstName: '',
