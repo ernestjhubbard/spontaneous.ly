10264148ba36797732ac5122557cb52b600918eb
juliehchung <juliehchung96@gmail.com>
2019-12-03 01:42:49 +0000

    Added styling for spontaneity points in upcoming activities

--- client/components/activity.jsx
+++ client/components/activity.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 function Activity(props) {
   const activityInfo = props.activityData;
+
   return (
     <div className="row border rounded m-3">
       <div className="activity-thumbnail border rounded m-1" style={{ backgroundImage: `url(assets/images/activity/${activityInfo.image}` }}></div>
-      <div className="">
+      <div className="m-3">
         <div className="">{activityInfo.activity}</div>
-        <div className="">{activityInfo.dateTime}</div>
-        <div className="">{activityInfo.points} Spontaneity Points</div>
+        <div className="text-muted">{activityInfo.dateTime}</div>
+        <div className="">
+          <p className="mb-0">
+            <small>
+              <span className="points d-inline-flex justify-content-center align-items-center mr-2">{activityInfo.points}</span>
+            Spontaneity Points
+            </small>
+          </p>
+        </div>
       </div>
     </div>
   );
