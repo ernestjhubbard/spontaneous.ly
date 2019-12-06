aec1e5eb82015e8d13d61a6cd20a5b0f2ab08978
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-12-06 00:43:45 +0000

    fixing the extra link

--- client/components/activity-detail.jsx
+++ client/components/activity-detail.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CancelModal from './cancel-modal';
-import { Link } from 'react-router-dom';
 
 class ActivityDetail extends React.Component {
   constructor(props) {
