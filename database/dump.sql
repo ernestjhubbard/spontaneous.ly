5f6a6d04b245b58f203bbf819c4231bcb17c3612
juliehchung <juliehchung96@gmail.com>
2019-12-06 20:42:51 +0000

    Update to reservations table in database

--- database/dump.sql
+++ database/dump.sql
@@ -164,7 +164,7 @@ CREATE TABLE `reservations` (
 
 LOCK TABLES `reservations` WRITE;
 /*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
-INSERT INTO `reservations` VALUES (1,88,0),(1,45,0),(1,47,1),(1,84,0),(1,56,1),(1,65,0),(1,21,0),(1,42,0),(1,33,1),(1,77,0),(1,58,0),(2,25,0);
+INSERT INTO `reservations` VALUES (4,89,0),(4,53,0),(4,39,0),(4,43,0),(4,11,0),(4,88,0),(4,87,0),(4,45,0),(4,47,0);
 /*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -205,4 +205,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-06 19:50:58
+-- Dump completed on 2019-12-06 20:42:01
