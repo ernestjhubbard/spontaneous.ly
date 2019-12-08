01846bdde6558a4b3aa1a63bba5390badb8628e1
ernest hubbard <ernhub89@gmail.com>
2019-12-08 09:13:04 -0800

    change user info backend

--- server/public/index.php
+++ server/public/index.php
@@ -12,13 +12,14 @@ switch ($request['path']) {
   case '/api/points':
   case '/api/users':
   case '/api/signup':
-  case '/api/update-password':
+  case '/api/update-account':
   case '/api/reservations':
   case '/api/upcoming-past-activities':
   case '/api/all-activities':
   case '/api/activity-details':
   case '/api/health-check':
     require_once "..${request['path']}.php";
+  break;
   default:
     throw new ApiError("Cannot ${request['method']} ${request['path']}", 404);
 }
