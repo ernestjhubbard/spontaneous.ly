deb5f8999000fc79f2c0bb4df0633fd89bab42a1
Vicky Huynhcao <vicky89@gmail.com>
2019-12-10 18:58:18 +0000

    Styled the accept button in pending list

--- client/components/friend.jsx
+++ client/components/friend.jsx
@@ -4,13 +4,14 @@ export default function Friend(props) {
   const friendImage = {
     backgroundImage: `url("/assets/images/users/${props.image}")`
   };
-  const userId = props.user;
   const recipientId = props.recipientId;
-  const senderId = props.senderId;
   let button;
-  const acceptReject = (<button>
-    <i className="far fa-check-circle" onClick={() => props.acceptRequest({ recipientId })}/>
-  </button>);
+  const acceptReject = (
+    <i
+      className="far fa-check-circle fa-2x adventure-card m-auto"
+      onClick={() => props.acceptRequest({ recipientId })}
+    />
+  );
   if (props.isAccepted === 1) {
     button = (
       <i
@@ -21,7 +22,7 @@ export default function Friend(props) {
       />
     );
   } else {
-    button = userId === senderId ? 'PENDING' : acceptReject;
+    button = acceptReject;
   }
   return (
     <div className="d-flex justify-content-between my-3">
