83d53b8a87884000965f6023c16de8a4f6b48209
juliehchung <juliehchung96@gmail.com>
2019-12-11 00:43:07 +0000

    Fixing bug to disallow users from returning to home from activities

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,15 +3,27 @@ import MenuNav from './menu-nav.jsx';
 import { Link } from 'react-router-dom';
 
 function Header(props) {
-  return (
-    <div className="header-bar d-flex sticky-top">
-      <Link to="/">
+  const currentUrl = new URL(window.location.href);
+  const currentPage = currentUrl.pathname;
+  if (currentPage === '/activity-list' || currentPage === '/activity-details') {
+    return (
+      <div className="header-bar d-flex sticky-top">
         <img className="logo-alone"
           src="assets/images/logo-alone.png" />
-      </Link>
-      <MenuNav signOut={props.signOut} currentView={props.currentView} user={props.user} />
-    </div>
-  );
+        <MenuNav signOut={props.signOut} currentView={props.currentView} user={props.user} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="header-bar d-flex sticky-top">
+        <Link to="/">
+          <img className="logo-alone"
+            src="assets/images/logo-alone.png" />
+        </Link>
+        <MenuNav signOut={props.signOut} currentView={props.currentView} user={props.user} />
+      </div>
+    );
+  }
 }
 
 export default Header;
