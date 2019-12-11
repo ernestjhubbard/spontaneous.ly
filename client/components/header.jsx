d8885acfeab746e707289c57173015ec341d7d61
juliehchung <juliehchung96@gmail.com>
2019-12-11 00:50:44 +0000

    Fixed bug to prevent users from cheating reroll

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,25 +5,25 @@ import { Link } from 'react-router-dom';
 function Header(props) {
   const currentUrl = new URL(window.location.href);
   const currentPage = currentUrl.pathname;
-  if (currentPage === '/activity-list' || currentPage === '/activity-details') {
-    return (
-      <div className="header-bar d-flex sticky-top">
+
+  const unclickable = (
+    <div className="header-bar d-flex sticky-top">
+      <img className="logo-alone"
+        src="assets/images/logo-alone.png" />
+    </div>
+  );
+
+  const clickable = (
+    <div className="header-bar d-flex sticky-top">
+      <Link to="/">
         <img className="logo-alone"
           src="assets/images/logo-alone.png" />
-        <MenuNav signOut={props.signOut} currentView={props.currentView} user={props.user} />
-      </div>
-    );
-  } else {
-    return (
-      <div className="header-bar d-flex sticky-top">
-        <Link to="/">
-          <img className="logo-alone"
-            src="assets/images/logo-alone.png" />
-        </Link>
-        <MenuNav signOut={props.signOut} currentView={props.currentView} user={props.user} />
-      </div>
-    );
-  }
+      </Link>
+      <MenuNav signOut={props.signOut} currentView={props.currentView} user={props.user} />
+    </div>
+  );
+
+  return (currentPage === '/activity-list' || currentPage === '/activity-details') ? unclickable : clickable;
 }
 
 export default Header;
