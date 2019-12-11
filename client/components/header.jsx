e71156b2eb828a9c4a10488c317303d7bac42689
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-12-11 00:54:42 +0000

    added more z-index

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Header(props) {
   return (
-    <div className="header-bar d-flex">
+    <div className="header-bar d-flex sticky-top">
       <Link to="/">
         <img className="logo-alone"
           src="assets/images/logo-alone.png" />
