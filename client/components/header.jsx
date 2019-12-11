c63789225cc533ee4b3a7714581dca8bd429b015
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-12-11 02:03:06 +0000

    added new class to navbar

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Header(props) {
   return (
-    <div className="header-bar d-flex sticky-top">
+    <div className="header-bar d-flex navbar sticky-top">
       <Link to="/">
         <img className="logo-alone"
           src="assets/images/logo-alone.png" />
