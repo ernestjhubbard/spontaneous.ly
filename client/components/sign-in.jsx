5527b6b53b820f116c7e487bea955fe79624acec
juliehchung <juliehchung96@gmail.com>
2019-12-11 23:48:58 +0000

    Fixed login validation bug, , user now sees validation message when credentials are incorrect

--- client/components/sign-in.jsx
+++ client/components/sign-in.jsx
@@ -5,7 +5,8 @@ class SignIn extends React.Component {
     super(props);
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      isValid: null
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,7 +17,12 @@ class SignIn extends React.Component {
   }
 
   handleSubmit() {
-    this.props.signIn(this.state);
+    this.props.signIn({ email: this.state.email, password: this.state.password });
+    if (this.props.user) {
+      this.setState({ isValid: true });
+    } else {
+      this.setState({ isValid: false });
+    }
   }
 
   componentDidUpdate(prevProps) {
@@ -26,27 +32,27 @@ class SignIn extends React.Component {
   }
 
   render() {
+    const validation = this.state.isValid === false ? 'is-invalid' : null;
     return (
       <div className="container align-center my-5">
         <h4 className="text-center font-weight-bold mb-3">Sign In</h4>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="email">Email</label>
-          <input className="input-font form-control form-control-lg text-center mb-3"
+          <input className={`input-font form-control form-control-lg ${validation} text-center mb-3`}
             name="email"
             onChange={this.handleChange}
             value={this.state.email}
             type="text"
-            placeholder="Email Address"
-          ></input>
+            placeholder="Email Address" required></input>
           <label htmlFor="password">Password</label>
-          <input className="input-font form-control form-control-lg text-center"
+          <input className={`input-font form-control form-control-lg ${validation} text-center`}
             name="password"
             onChange={this.handleChange}
             value={this.state.password}
             type="password"
-            placeholder="Password"
-          ></input>
+            placeholder="Password" required></input>
           <button type="submit" className="spon-button rounded text-white w-100" value="Submit">Submit</button>
+          <div className="invalid-feedback text-center">Invalid email or password. Please try again.</div>
         </form>
       </div>
     );
